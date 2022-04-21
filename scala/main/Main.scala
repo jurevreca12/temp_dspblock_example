5e42750914d4ce4d97d42ef0e8a6eb9592d3f41c
jvreca <jure.vreca@ijs.si>
2022-04-21 16:40:14 +0200

    Added FIRBlock.

--- scala/main/Main.scala
+++ scala/main/Main.scala
@@ -8,17 +8,21 @@
 package chipyard.example
 
 import chisel3._
+import dspblocks._
 import freechips.rocketchip.config.Parameters
-import freechips.rocketchip.diplomacy.LazyModule
+import freechips.rocketchip.diplomacy._
 /**
  * An object extending App to generate the Verilog code.
  */
 object Main {
   def main(args: Array[String]): Unit = {
     println("asdasdasdAASDASDA")
-    implicit val p: Parameters = Parameters.empty
-    val lazyModule = LazyModule(new StreamingPassthroughTop)
-    (new chisel3.stage.ChiselStage).emitVerilog(lazyModule.module, Array("-td","gen/"))  
+    implicit val valName = ValName("Axi4Test")
+    (new chisel3.stage.ChiselStage).emitVerilog(LazyModule(new AXI4FIRBlock(1, 8)(Parameters.empty) with AXI4StandaloneBlock).module, 
+                                                Array("-td","gen/"))
+    //implicit val p: Parameters = Parameters.empty
+    //val lazyModule = LazyModule(new StreamingPassthroughTop)
+    //(new chisel3.stage.ChiselStage).emitVerilog(lazyModule.module, Array("-td","gen/"))  
   }
 }
 
--- scala/main/StreamingTop.scala
+++ scala/main/StreamingTop.scala
@@ -8,7 +8,5 @@ import freechips.rocketchip.config.Parameters
 class StreamingPassthroughTop(implicit p: Parameters) extends LazyModule {
   val m1 = LazyModule(new StreamingPassthroughBlock(UInt(4.W)))
 
-  lazy val module = new LazyModuleImp(this) {
-    m1.module.in = 3.U
-  }
+  lazy val module = new LazyModuleImp(this) { m1 }
 }
