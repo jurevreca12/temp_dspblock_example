bcfca1a7648898e745fb6a6e675e3c4e2c2c8fef
jvreca <jure.vreca@ijs.si>
2022-04-25 15:46:46 +0200

    Diplomatic adder generating.

--- scala/main/Main.scala
+++ scala/main/Main.scala
@@ -11,18 +11,34 @@ import chisel3._
 import dspblocks._
 import freechips.rocketchip.config.Parameters
 import freechips.rocketchip.diplomacy._
+import java.io.File
+import java.io.FileWriter
+
 /**
  * An object extending App to generate the Verilog code.
  */
 object Main {
   def main(args: Array[String]): Unit = {
-    println("asdasdasdAASDASDA")
-    implicit val valName = ValName("Axi4Test")
-    (new chisel3.stage.ChiselStage).emitVerilog(LazyModule(new AXI4FIRBlock(1, 8)(Parameters.empty) with AXI4StandaloneBlock).module, 
-                                                Array("-td","gen/"))
+    //println("asdasdasdAASDASDA")
+    //implicit val valName = ValName("Axi4Test")
+    //println((new chisel3.stage.ChiselStage).emitVerilog(() => LazyModule(new AXI4FIRBlock(1, 8)(Parameters.empty) with AXI4StandaloneBlock).module))
+    //(new chisel3.stage.ChiselStage).emitVerilog(LazyModule(new AXI4FIRBlock(1, 8)(Parameters.empty) with AXI4StandaloneBlock).module, 
+    //                                            Array("-td","gen/"))
+    
+    //(new chisel3.stage.ChiselStage).emitVerilog(LazyModule(new Adder()), Array("-td", "gen/"))
+
     //implicit val p: Parameters = Parameters.empty
     //val lazyModule = LazyModule(new StreamingPassthroughTop)
     //(new chisel3.stage.ChiselStage).emitVerilog(lazyModule.module, Array("-td","gen/"))  
+    
+    val verilog = (new chisel3.stage.ChiselStage).emitVerilog(
+        LazyModule(new AdderTestHarness()(Parameters.empty)).module
+    )
+    //println(s"```verilog\n$verilog```")
+    //
+    val fileWriter = new FileWriter(new File("./gen/gen.v"))
+    fileWriter.write(verilog)
+    fileWriter.close()
   }
 }
 
