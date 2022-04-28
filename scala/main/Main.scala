0f9a21ed4e40f47faf55216da2940b0f0ea226ed
jvreca <jure.vreca@ijs.si>
2022-04-28 11:42:24 +0200

    APB example generating verilog.

--- scala/main/Main.scala
+++ scala/main/Main.scala
@@ -4,15 +4,26 @@ import chisel3._
 import freechips.rocketchip.config.Parameters
 import freechips.rocketchip.diplomacy._
 
+import java.io.File
+import java.io.FileWriter
+
+
 /**
  * An object extending App to generate the Verilog code.
  */
 object Main {
   def main(args: Array[String]): Unit = {
+    //(new chisel3.stage.ChiselStage).execute(args, Seq(ChiselGeneratorAnnotation(() => LazyModule(new MyWrapper()(Parameters.empty)).module)))
+
     val verilog = (new chisel3.stage.ChiselStage).emitVerilog(
-                   LazyModule(new AxiExampleTop()(Parameters.empty)).module
+                   LazyModule(new MyWrapper()(Parameters.empty)).module
     )
-    println(s"```verilog\n$verilog```")
+    //println(s"```verilog\n$verilog```")
+
+    val fileWriter = new FileWriter(new File("./gen/gen.v"))
+    fileWriter.write(verilog)
+    fileWriter.close()
+
   }
 }
 
